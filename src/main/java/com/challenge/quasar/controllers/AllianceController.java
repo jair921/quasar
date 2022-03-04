a4aa1dfee595deb1888454fe2baf1b16d595b47d
Jair Andres Martinez Manzano <jair.martinez@evertecinc.com>
2022-03-03 23:49:17 -0500

    feat: add response for store split

--- src/main/java/com/challenge/quasar/controllers/AllianceController.java
+++ src/main/java/com/challenge/quasar/controllers/AllianceController.java
@@ -1,16 +1,14 @@
 package com.challenge.quasar.controllers;
 
 import com.challenge.quasar.domain.alliance.entities.IncomingCommunication;
+import com.challenge.quasar.domain.alliance.entities.Satellite;
 import com.challenge.quasar.domain.alliance.services.AllianceService;
 import com.challenge.quasar.domain.message.exceptions.MessageException;
 import com.challenge.quasar.domain.position.exceptions.PositionException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class AllianceController {
@@ -27,5 +25,12 @@ public class AllianceController {
         }catch (MessageException | PositionException exception){
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @RequestMapping(path = "/topsecret_split/{satelliteName}")
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity topSecret(@RequestBody Satellite satellite, @PathVariable String satelliteName)
+    {
+        return null;
     }
 }
