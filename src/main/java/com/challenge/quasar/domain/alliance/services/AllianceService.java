7554e479eeed59682148f957133e5742c001bea0
Jair Andres Martinez Manzano <jair.martinez@evertecinc.com>
2022-03-03 23:24:30 -0500

    feat: add exceptions

--- src/main/java/com/challenge/quasar/domain/alliance/services/AllianceService.java
+++ src/main/java/com/challenge/quasar/domain/alliance/services/AllianceService.java
@@ -2,9 +2,12 @@ package com.challenge.quasar.domain.alliance.services;
 
 import com.challenge.quasar.domain.alliance.entities.IncomingCommunication;
 import com.challenge.quasar.domain.alliance.entities.OutgoingCommunication;
+import com.challenge.quasar.domain.message.exceptions.MessageException;
+import com.challenge.quasar.domain.position.exceptions.PositionException;
 import com.challenge.quasar.domain.position.services.PositionService;
 import com.challenge.quasar.domain.message.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +19,21 @@ public class AllianceService {
     @Autowired
     MessageService messageService;
 
-    public OutgoingCommunication receiveCommunication(IncomingCommunication incomingCommunication)
-    {
+    @Autowired
+    Environment env;
+
+    public OutgoingCommunication receiveCommunication(IncomingCommunication incomingCommunication) throws PositionException, MessageException {
+
+        int numSatellites = Integer.parseInt(env.getProperty("satellites.size"));
+
+        if(incomingCommunication.getSatellites().size() < numSatellites){
+            throw new PositionException("You need at least " + numSatellites + " satellites to locate the spaceship.");
+        }
+
+        if(incomingCommunication.secretMessages().size() < numSatellites){
+            throw new MessageException("You need at least " + numSatellites + " messages to decode the secret.");
+        }
+
         double[] position = positionService.getLocation(incomingCommunication.distances());
         String secretMessage = messageService.getMessage(incomingCommunication.secretMessages());
 
---
+++ src/main/java/com/challenge/quasar/domain/position/exceptions/PositionException.java
@@ -0,0 +1,8 @@
+package com.challenge.quasar.domain.position.exceptions;
+
+public class PositionException extends Exception{
+
+    public PositionException(String message) {
+        super(message);
+    }
+}
